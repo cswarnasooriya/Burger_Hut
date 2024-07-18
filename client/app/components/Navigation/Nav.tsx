ee3f1881c2ca4af1f95c58f7879dce3e8692faeb
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-07-19 01:47:53 +0530

    Nav section completed

--- client/app/components/Navigation/Nav.tsx
+++ client/app/components/Navigation/Nav.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link'
 import React from 'react'
+import { BiCycling, BiShoppingBag } from 'react-icons/bi'
 import { FaBurger } from 'react-icons/fa6'
+import { HiBars3BottomRight } from 'react-icons/hi2'
 
 const Nav = () => {
   return (
@@ -11,8 +14,51 @@ const Nav = () => {
         <div className="items-center flex space-x-3">
           <FaBurger className="w-[1.2rem] h-[1.2rem] sm:w-[1.4rem] sm:h-[1.4rem] text-orange-500 " />
           <h1 className="text-[20px] sm:text-[30px] font-semibold">Burger Hut</h1>
+        </div>
+
+        {/* Nav links */}
+        <ul className="hidden lg:flex items-center space-x-10">
+          <li className="text-[20px] font-medium hover:text-red-600 ">
+            <Link href="/">Home</Link>
+          </li>
+
+          <li className="text-[20px] font-medium hover:text-red-600 ">
+            <Link href="/menu">Menu</Link>
+          </li>
+
+          <li className="text-[20px] font-medium hover:text-red-600 ">
+            <Link href="/shop">Shop</Link>
+          </li>
+
+          <li className="text-[20px] font-medium hover:text-red-600 ">
+            <Link href="/blog">Blog</Link>
+          </li>
+
+          <li className="text-[20px] font-medium hover:text-red-600 ">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        {/* buttons */}
+        <div className="flex items-center space-x-4">
+          <button className="py-2 px-6 sm:px-8 sm:py-3 text-[14px] sm:text-[16px] bg-blue-950 transition-all duration-200 hover:bg-red-600 flex items-center rounded-md space-x-2 text-white">
+            <span>
+              <BiCycling className="w-[1.3rem] h-[1.3rem] sm:w-[1.7rem] sm:h-[1.7rem] " />
+            </span>
+            <span className="font-bold">
+              Order Now
+            </span>
+          </button>
+
+          <button className="sm:px-6 sm:py-3 px-4 py-2 hover:bg-green-700 transition-all duration-200 bg-orange-600 flex items-center rounded-md text-white">
+            <BiShoppingBag className="w-[1.3rem] h-[1.3rem] sm:w-[1.7rem] sm:h-[1.7rem] " />
+          </button>
+
+          <HiBars3BottomRight className="lg:hidden w-[2rem] h-[2rem] text-black" />
 
         </div>
+
+
       </div>
       
     </div>
